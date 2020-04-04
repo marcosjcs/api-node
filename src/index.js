669e1e8622ff74b526105b5e93c3ef0b5d2247f1
marcosjcs <jcs.marcos13@gmail.com>
2020-04-04 17:17:53 -0300

    Register user

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+require('./controllers/authController')(app);
+app.get('/', (req, res)=>{
+    res.send('Ok');
+})
+app.listen(3000, ()=> console.log('Server started'));
