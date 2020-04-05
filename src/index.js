8061d899e42e32edec5e83b910057f14ca2b68bf
marcosjcs <jcs.marcos13@gmail.com>
2020-04-04 21:16:38 -0300

    JWT Authentication

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 require('./controllers/authController')(app);
-app.get('/', (req, res)=>{
-    res.send('Ok');
-})
+require('./controllers/projectController')(app);
+
 app.listen(3000, ()=> console.log('Server started'));
