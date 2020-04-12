995ce1e8f3310e13c580880a41c999a299a89527
marcosjcs <jcs.marcos13@gmail.com>
2020-04-12 20:56:41 -0300

    Reset password

--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/apinoderest', 
-{ useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true })
+{ useCreateIndex: true, useFindAndModify: false, useNewUrlParser: true, useUnifiedTopology: true })
 
 mongoose.Promise = global.Promise;
 
